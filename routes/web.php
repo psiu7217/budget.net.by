d5e445e7d2671576cabce88415bdebf67623efb7
daniska <daniska@scnsoft.com>
2022-11-15 23:13:56 +0300

    Only auth users

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/dashboard', function () {
         return view('dashboard');
-    });
+    })->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
