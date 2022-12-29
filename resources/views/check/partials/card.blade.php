61deee0efc0f365b48820ca7e7263b48992183be
daniska <daniska@scnsoft.com>
2022-12-29 22:44:00 +0300

    Toggle buttons

--- resources/views/check/partials/card.blade.php
+++ resources/views/check/partials/card.blade.php
@@ -1,5 +1,5 @@
 <section class="space-y-6">
-    <header>
+    <header onclick="nextToggle(this)">
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 flex justify-between">
             <div class="flex">
                 <div class="mt-1 text-sm text-gray-600 dark:text-gray-400 mr-2">{{  $check->category->group->title  }} - {{  $check->category->title  }} - </div>
@@ -13,7 +13,7 @@
         </div>
     </header>
 
-    <div class="flex justify-between">
+    <div class="flex justify-between none">
         <a href="{{ route('check.edit', $check->id) }}" class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150">
             {{ __('Edit Check') }}
         </a>
