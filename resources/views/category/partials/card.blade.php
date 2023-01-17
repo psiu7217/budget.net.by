d76bf004bd7d54e245ac41728e3a36c39970c1b4
daniska <daniska@scnsoft.com>
2023-01-17 14:12:53 +0300

    Update home page

--- resources/views/category/partials/card.blade.php
+++ resources/views/category/partials/card.blade.php
@@ -2,7 +2,7 @@
     <header class="border-t py-2 border-gray-100 dark:border-gray-700">
         <a href="{{ route('category.edit', $category->id) }}" class="flex justify-between items-center">
             <span class="text-gray-400 dark:text-gray-200">{{  $category->title  }}</span>
-            <span class="text-gray-400 dark:text-gray-200">{{  $category->plans->sortByDesc('created_at')->first()->cash|round()  }} BYN</span>
+            <span class="text-gray-400 dark:text-gray-200">{{  $category->plans->sortByDesc('created_at')->first()->cash|round(0)  }} BYN</span>
         </a>
     </header>
 
